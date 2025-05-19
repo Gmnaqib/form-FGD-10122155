bd0339544a0ac586a60558839ade02b2923aef44
Gmnaqib <gumilarmn16@gmail.com>
2025-05-19 23:02:03 +0700

    fix bug text and hint

--- app/src/main/java/gumilar/unikom/formulirfgdapps/DetailActivity.kt
+++ app/src/main/java/gumilar/unikom/formulirfgdapps/DetailActivity.kt
@@ -5,16 +5,37 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import gumilar.unikom.formulirfgdapps.databinding.ActivityDetailBinding
+import gumilar.unikom.formulirfgdapps.model.Participant
 
 class DetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_detail)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val participant = intent.getParcelableExtra<Participant>("participant")
+        if (participant != null) {
+            binding.txtNamaResult.text = participant.name
+            binding.txtPhoneResult.text = participant.phone
+            binding.txtEmailResult.text = participant.email
+            binding.txtGenderResult.text = participant.gender
+            binding.txtCategoryResult.text = participant.category
+            binding.txtSkillsetResult.text = participant.skillset.joinToString(", ")
+        }
+
+        binding.btnInfoDeveloper.setOnClickListener {
+            val bottomSheet = InfoDeveloper()
+            bottomSheet.show(supportFragmentManager, "InfoDeveloper")
         }
     }
 }
---
+++ app/src/main/java/gumilar/unikom/formulirfgdapps/InfoDeveloper.kt
@@ -0,0 +1,15 @@
+package gumilar.unikom.formulirfgdapps
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class InfoDeveloper: BottomSheetDialogFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.info_developer, container, false)
+    }
+}
